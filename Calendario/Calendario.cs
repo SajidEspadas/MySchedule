b6ed87449e2c085f5887d853a9abbf6190d43295
Chrisare18 <chrisare17@gmail.com>
2022-05-15 12:20:11 -0700

    Se creo el forms de calendario

---
+++ Calendario/Calendario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendario
+{
+    public partial class Calendario : Form
+    {
+        public Calendario()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void Calendario_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
