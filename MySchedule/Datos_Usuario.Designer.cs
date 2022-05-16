ed2d2356e30cd92b230569ac729ddcba18212a10
victor.olivares193@tectijuana.edu.mx <victor.olivares193@tectijuana.edu.mx>
2022-05-15 19:32:48 -0700

    Creados Forms de Datos_Usuario y Horarios

---
+++ MySchedule/Datos_Usuario.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace MySchedule
+{
+    partial class Datos_Usuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBox_Datos_del_Usuarioo = new System.Windows.Forms.GroupBox();
+            this.Label_ID_Usuario = new System.Windows.Forms.Label();
+            this.TextBox_ID_Usuario = new System.Windows.Forms.TextBox();
+            this.Label_Correo_Usuario = new System.Windows.Forms.Label();
+            this.Label_Nombre_Usuario = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Label_Telefono_Usuario = new System.Windows.Forms.Label();
+            this.Label_Apellido_Usuario = new System.Windows.Forms.Label();
+            this.TextBox_Nombre_Usuario = new System.Windows.Forms.TextBox();
+            this.TextBox_Apellido_Usuario = new System.Windows.Forms.TextBox();
+            this.TextBox_Telefono_Usuario = new System.Windows.Forms.TextBox();
+            this.TextBox_Correo_Usuario = new System.Windows.Forms.TextBox();
+            this.GroupBox_Datos_del_Usuarioo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBox_Datos_del_Usuarioo
+            // 
+            this.GroupBox_Datos_del_Usuarioo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.TextBox_Correo_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.TextBox_Telefono_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.TextBox_Apellido_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.TextBox_Nombre_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.Label_ID_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.TextBox_ID_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.Label_Correo_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.Label_Nombre_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.label2);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.Label_Telefono_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Controls.Add(this.Label_Apellido_Usuario);
+            this.GroupBox_Datos_del_Usuarioo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GroupBox_Datos_del_Usuarioo.Location = new System.Drawing.Point(52, 30);
+            this.GroupBox_Datos_del_Usuarioo.Name = "GroupBox_Datos_del_Usuarioo";
+            this.GroupBox_Datos_del_Usuarioo.Size = new System.Drawing.Size(619, 418);
+            this.GroupBox_Datos_del_Usuarioo.TabIndex = 11;
+            this.GroupBox_Datos_del_Usuarioo.TabStop = false;
+            this.GroupBox_Datos_del_Usuarioo.Text = "Datos del Usuario";
+            // 
+            // Label_ID_Usuario
+            // 
+            this.Label_ID_Usuario.AutoSize = true;
+            this.Label_ID_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_ID_Usuario.Location = new System.Drawing.Point(74, 46);
+            this.Label_ID_Usuario.Name = "Label_ID_Usuario";
+            this.Label_ID_Usuario.Size = new System.Drawing.Size(32, 24);
+            this.Label_ID_Usuario.TabIndex = 0;
+            this.Label_ID_Usuario.Text = "ID:";
+            // 
+            // TextBox_ID_Usuario
+            // 
+            this.TextBox_ID_Usuario.Enabled = false;
+            this.TextBox_ID_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_ID_Usuario.Location = new System.Drawing.Point(164, 41);
+            this.TextBox_ID_Usuario.Name = "TextBox_ID_Usuario";
+            this.TextBox_ID_Usuario.Size = new System.Drawing.Size(298, 29);
+            this.TextBox_ID_Usuario.TabIndex = 1;
+            // 
+            // Label_Correo_Usuario
+            // 
+            this.Label_Correo_Usuario.AutoSize = true;
+            this.Label_Correo_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Correo_Usuario.Location = new System.Drawing.Point(74, 187);
+            this.Label_Correo_Usuario.Name = "Label_Correo_Usuario";
+            this.Label_Correo_Usuario.Size = new System.Drawing.Size(73, 24);
+            this.Label_Correo_Usuario.TabIndex = 8;
+            this.Label_Correo_Usuario.Text = "Correo:";
+            // 
+            // Label_Nombre_Usuario
+            // 
+            this.Label_Nombre_Usuario.AutoSize = true;
+            this.Label_Nombre_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Nombre_Usuario.Location = new System.Drawing.Point(74, 79);
+            this.Label_Nombre_Usuario.Name = "Label_Nombre_Usuario";
+            this.Label_Nombre_Usuario.Size = new System.Drawing.Size(84, 24);
+            this.Label_Nombre_Usuario.TabIndex = 2;
+            this.Label_Nombre_Usuario.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // Label_Telefono_Usuario
+            // 
+            this.Label_Telefono_Usuario.AutoSize = true;
+            this.Label_Telefono_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Telefono_Usuario.Location = new System.Drawing.Point(74, 150);
+            this.Label_Telefono_Usuario.Name = "Label_Telefono_Usuario";
+            this.Label_Telefono_Usuario.Size = new System.Drawing.Size(90, 24);
+            this.Label_Telefono_Usuario.TabIndex = 6;
+            this.Label_Telefono_Usuario.Text = "Telefono:";
+            // 
+            // Label_Apellido_Usuario
+            // 
+            this.Label_Apellido_Usuario.AutoSize = true;
+            this.Label_Apellido_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Apellido_Usuario.Location = new System.Drawing.Point(74, 117);
+            this.Label_Apellido_Usuario.Name = "Label_Apellido_Usuario";
+            this.Label_Apellido_Usuario.Size = new System.Drawing.Size(84, 24);
+            this.Label_Apellido_Usuario.TabIndex = 4;
+            this.Label_Apellido_Usuario.Text = "Apellido:";
+            // 
+            // TextBox_Nombre_Usuario
+            // 
+            this.TextBox_Nombre_Usuario.Enabled = false;
+            this.TextBox_Nombre_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_Nombre_Usuario.Location = new System.Drawing.Point(164, 74);
+            this.TextBox_Nombre_Usuario.Name = "TextBox_Nombre_Usuario";
+            this.TextBox_Nombre_Usuario.Size = new System.Drawing.Size(298, 29);
+            this.TextBox_Nombre_Usuario.TabIndex = 9;
+            // 
+            // TextBox_Apellido_Usuario
+            // 
+            this.TextBox_Apellido_Usuario.Enabled = false;
+            this.TextBox_Apellido_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_Apellido_Usuario.Location = new System.Drawing.Point(164, 112);
+            this.TextBox_Apellido_Usuario.Name = "TextBox_Apellido_Usuario";
+            this.TextBox_Apellido_Usuario.Size = new System.Drawing.Size(298, 29);
+            this.TextBox_Apellido_Usuario.TabIndex = 10;
+            // 
+            // TextBox_Telefono_Usuario
+            // 
+            this.TextBox_Telefono_Usuario.Enabled = false;
+            this.TextBox_Telefono_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_Telefono_Usuario.Location = new System.Drawing.Point(164, 150);
+            this.TextBox_Telefono_Usuario.Name = "TextBox_Telefono_Usuario";
+            this.TextBox_Telefono_Usuario.Size = new System.Drawing.Size(298, 29);
+            this.TextBox_Telefono_Usuario.TabIndex = 11;
+            // 
+            // TextBox_Correo_Usuario
+            // 
+            this.TextBox_Correo_Usuario.Enabled = false;
+            this.TextBox_Correo_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_Correo_Usuario.Location = new System.Drawing.Point(164, 187);
+            this.TextBox_Correo_Usuario.Name = "TextBox_Correo_Usuario";
+            this.TextBox_Correo_Usuario.Size = new System.Drawing.Size(298, 29);
+            this.TextBox_Correo_Usuario.TabIndex = 12;
+            // 
+            // Datos_Usuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 516);
+            this.Controls.Add(this.GroupBox_Datos_del_Usuarioo);
+            this.Name = "Datos_Usuario";
+            this.Text = "Datos_Usuario";
+            this.GroupBox_Datos_del_Usuarioo.ResumeLayout(false);
+            this.GroupBox_Datos_del_Usuarioo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GroupBox_Datos_del_Usuarioo;
+        private System.Windows.Forms.TextBox TextBox_Correo_Usuario;
+        private System.Windows.Forms.TextBox TextBox_Telefono_Usuario;
+        private System.Windows.Forms.TextBox TextBox_Apellido_Usuario;
+        private System.Windows.Forms.TextBox TextBox_Nombre_Usuario;
+        private System.Windows.Forms.Label Label_ID_Usuario;
+        private System.Windows.Forms.TextBox TextBox_ID_Usuario;
+        private System.Windows.Forms.Label Label_Correo_Usuario;
+        private System.Windows.Forms.Label Label_Nombre_Usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Label_Telefono_Usuario;
+        private System.Windows.Forms.Label Label_Apellido_Usuario;
+    }
+}
---
+++ MySchedule/Datos_Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySchedule
+{
+    public partial class Datos_Usuario : Form
+    {
+        public Datos_Usuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
